22d0acdf7b43f4afb04211345bcd4f51051418fe
Maria-Juliana-Saavedra-Mejia <saavedra.mariaju@gmail.com>
2025-04-09 20:14:52 -0500

    feat :sparkles: menus update

---
+++ Dia1/script/campers.js
@@ -0,0 +1,186 @@
+// #####################################
+// Ejercicios DIA 1
+// #####################################
+
+export function inscripcion(){
+    idn=prompt("Digite su numero de Identificación 🎂");
+    nombre=prompt("Digite sus nombre 🤳");
+    apellido=prompt("Digite sus apellidos 😜");
+    direccion=prompt("Ingrese su dirección 🐱‍🏍");
+    acudiente=prompt("Ingrese el nombre de su acudiente 🐱‍👓");
+    numcel=prompt("Ingrese su número de celular 👍");
+    numfijo=prompt("Ingrese su número de teléfono fijo 😉");
+    curso="";
+}
+
+let campers=[
+    {
+        "ID": 6677889501,
+        "Nombre": "Andrés Felipe",
+        "Apellido": "Garcia Vargas",
+        "Direccion": "Cra 15 #18-09",
+        "Acudiente": "María Garcia",
+        "Numero de celular": 3152334455,
+        "Numero de telefono fijo": 6123456789,
+        "Estado": {
+            "En proceso": false,
+            "Inscrito": true,
+            "Aprobado": true,
+            "Rechazado": false,
+            "Cursando": false,
+            "Graduado": false,
+            "Expulsado": false,
+            "Retirado": false
+        },
+        "Riesgo": true,
+        "grupo": "P_2",
+        "notas": {
+            "modulo1": 15.533333333333333,
+            "modulo2": 90.0,
+            "modulo3": 0.0,
+            "modulo4": 0.0,
+            "modulo5": 0.0
+        }
+    },
+    {
+        "ID": 346436,
+        "Nombre": "fguhu",
+        "Apellido": "kdfjshsth",
+        "Direccion": "jkhnkgfgn",
+        "Acudiente": "fkgjrtjgr",
+        "Numero de celular": 4375348753,
+        "Numero de telefono fijo": 454698734,
+        "Estado": {
+            "En proceso": true,
+            "Inscrito": true,
+            "Aprobado": true,
+            "Rechazado": false,
+            "Cursando": false,
+            "Graduado": false,
+            "Expulado": false,
+            "Retirado": false
+        },
+        "Riesgo": false,
+        "grupo": "M_2",
+        "notas": {
+            "modulo1": 100.0,
+            "modulo2": 0.0,
+            "modulo3": 0.0,
+            "modulo4": 0.0,
+            "modulo5": 0.0
+        }
+    },
+    {
+        "ID": 1,
+        "Nombre": "a",
+        "Apellido": "a",
+        "Direccion": "a",
+        "Acudiente": "a",
+        "Numero de celular": 1,
+        "Numero de telefono fijo": 1,
+        "Estado": {
+            "En proceso": false,
+            "Inscrito": true,
+            "Aprobado": true,
+            "Rechazado": false,
+            "Cursando": false,
+            "Graduado": false,
+            "Expulado": false,
+            "Retirado": false
+        },
+        "Riesgo": false,
+        "grupo": "",
+        "notas": {
+            "modulo1": 0.0,
+            "modulo2": 0.0,
+            "modulo3": 0.0,
+            "modulo4": 0.0,
+            "modulo5": 0.0
+        }
+    },
+    {
+        "ID": 1,
+        "Nombre": "dssd",
+        "Apellido": "dsds",
+        "Direccion": "ddsed",
+        "Acudiente": "dfsfs",
+        "Numero de celular": 43434,
+        "Numero de telefono fijo": 43435,
+        "Estado": {
+            "En proceso": false,
+            "Inscrito": true,
+            "Aprobado": true,
+            "Rechazado": false,
+            "Cursando": false,
+            "Graduado": false,
+            "Expulado": false,
+            "Retirado": false
+        },
+        "Riesgo": false,
+        "grupo": "",
+        "notas": {
+            "modulo1": 0.0,
+            "modulo2": 0.0,
+            "modulo3": 0.0,
+            "modulo4": 0.0,
+            "modulo5": 0.0
+        }
+    },
+    {
+        "ID": 1234,
+        "Nombre": "Sara",
+        "Apellido": "Diaz",
+        "Direccion": "Cr 34",
+        "Acudiente": "Adriana Rodriguez",
+        "Numero de celular": 34567897,
+        "Numero de telefono fijo": 6543234567,
+        "Estado": {
+            "En proceso": false,
+            "Inscrito": true,
+            "Aprobado": false,
+            "Rechazado": false,
+            "Cursando": false,
+            "Graduado": false,
+            "Expulado": false,
+            "Retirado": false
+        },
+        "Riesgo": false,
+        "grupo": "",
+        "notas": {
+            "modulo1": 0.0,
+            "modulo2": 0.0,
+            "modulo3": 0.0,
+            "modulo4": 0.0,
+            "modulo5": 0.0
+        }
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Desarrollado por : Maria Juliana Saavedra Mejia - C.C.1.097.100.816
--- Dia1/script/script.js
+++ Dia1/script/script.js
@@ -1,31 +1,36 @@
-// #####################################
-// Ejercicios DIA 1
-// #####################################
+//#####################################// 
+// Ejercicios DIA 1// 
+// #####################################// 
 
+// Importaciones// 
+import * as menus from './menus.js'
+import * as campers from './campers.js'
+import * as trainers from './trainers.js'
 
+// muestra del menu principal// 
+menus.menuPrincipal();
+// elige del menu principal// 
+let n=prompt("Digite su elección de como quiere ingresar 👀")
 
+if (n==1){
+    // muestra del menu camper//
+    menus.menuEstudiante();
+    let n1=prompt("Digite el número de lo que quiere realizar ✔")
+    if (n1==1){
+        campers.inscripcion()
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}else if (n==2){
+    // muestra del menu trainer// 
+    menus.menuTrainer();
+    let n2=prompt("Digite el número correspondiente a su perfil 😆")
+}else if (n==3){
+    // muestra del menu Coordinador// 
+    menus.menuCoordinador();
+    let n3=prompt("Digite el número de lo que quiere realizar 😎")
+}else{
+    alert("Eleccion Incorrecta")
+}
 
 
 
