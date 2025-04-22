c5ddebf99ed695d8ee425a821544a3cc0309ff98
Maria-Juliana-Saavedra-Mejia <saavedra.mariaju@gmail.com>
2025-04-22 18:21:00 -0500

    feat :sparkles: final result

--- ApiRepaso/script/script.js
+++ ApiRepaso/script/script.js
@@ -2,7 +2,28 @@ function BuscarPoke(){
     let pokes = new XMLHttpRequest();
     let numPokemon = document.getElementById("fondo__formBusqueda--formtext").value;
     let link= `https://pokeapi.co/api/v2/pokemon/${numPokemon}`;
-    //Primer paso es crear(abrir) la solicitud
+
+    if (numPokemon==0){
+        numPokemon=1
+        let link= `https://pokeapi.co/api/v2/pokemon/${numPokemon}`;
+        //Primer paso es crear(abrir) la solicitud
+        pokes.open('GET',link,true);
+        //Segundo paso es decir qué hacer con esa solicitud
+        pokes.onreadystatechange = function(){
+        if(this.readyState === 4 && this.status ===200 ){
+            let respuesta = JSON.parse(this.responseText);
+            console.log(respuesta["sprites"]["other"]["showdown"]["front_default"]);
+            printPoke(respuesta);
+        }else{
+            printPoke({ response: "error" });  // <-- Aquí envías un "objeto de error"
+        }
+    };
+    //El tercer paso es ejecutar la solicitud
+    pokes.send();
+    }
+    else
+    {
+        //Primer paso es crear(abrir) la solicitud
     pokes.open('GET',link,true);
     //Segundo paso es decir qué hacer con esa solicitud
     pokes.onreadystatechange = function(){
@@ -10,26 +31,42 @@ function BuscarPoke(){
             let respuesta = JSON.parse(this.responseText);
             console.log(respuesta["sprites"]["other"]["showdown"]["front_default"]);
             printPoke(respuesta);
+        }else{
+            printPoke({ response: "error" });  // <-- Aquí envías un "objeto de error"
         }
     };
     //El tercer paso es ejecutar la solicitud
     pokes.send();
+    }
+
 }
 
 BuscarPoke();
 
 function MenosId(){
-
+    let input = document.getElementById("fondo__formBusqueda--formtext");
+    let numPokemon = parseInt(input.value, 10);
+    if(numPokemon > 1) {
+        numPokemon -= 1;
+        input.value = numPokemon;
+        BuscarPoke();
+    }
 }
+
+
 function MasId(){
-    
+    let input = document.getElementById("fondo__formBusqueda--formtext");
+    let numPokemon = parseInt(input.value, 10);
+    numPokemon += 1;
+    input.value = numPokemon;
+    BuscarPoke();
 }
-
 
 function printPoke(data){
     let informacionHTML = document.getElementById('fondo__poquemonInfo--name&image');
     if(data.response == "error"){
-        informacionHTML.innerHTML=`<p class="nada">Esto no funcionó :sadfeis:</p>`
+        informacionHTML.innerHTML=`<p class="nada">Error este pokemon no existe</p>`
+
     }else{
         informacionHTML.innerHTML=`
         <img class="foto" src=${data["sprites"]["other"]["showdown"]["front_default"]} />
@@ -50,4 +87,14 @@ input.addEventListener("keydown",function(event){
         BuscarPoke()
     }
 
+})
+
+const inputs= document.getElementById("fondo__formBusqueda--prev");
+inputs.addEventListener("click",function(){
+     MenosId()
+})
+
+const inputss= document.getElementById("fondo__formBusqueda--next");
+inputss.addEventListener("click",function(){
+     MasId()
 })
