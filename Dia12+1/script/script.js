a441778adcc42034ebdd65b5de759d8ae77a942f
Maria-Juliana-Saavedra-Mejia <saavedra.mariaju@gmail.com>
2025-05-05 18:22:52 -0500

    feat :sparkles: update day 12+1

--- Dia12+1/script/script.js
+++ Dia12+1/script/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const datosContenedor = document.querySelector('.opciones');
 
     async function fetchData(){
-        const res = await fetch('https://66df3340de4426916ee3dd7e.mockapi.io/tareas');
+        const res = await fetch('https://68163e4332debfe95dbddca2.mockapi.io/exampleclass');
         data = await res.json();
         return data;
     }
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         datosContenedor.innerHTML=``;
         data.forEach(cap =>{
             const capDiv=document.createElement('div');
-            if(cap.status="On hold"){
+            if(cap.status==="On hold"){
                 capDiv.innerHTML=`<div class="capsula">
             <div class="infoText">
                 <p>${cap.task}</p>
@@ -26,6 +26,21 @@ document.addEventListener('DOMContentLoaded',()=>{
 
             </div>
         </div>`;
+            }else if(cap.status==="ready"){
+                capDiv.innerHTML=`<div class="capsulaTerminado">
+                <div class="infoText">
+                    <p class="cambioTarea">${cap.task}</p>
+                </div>
+                <div class="botones">
+                    <div class="terminado">
+                        <img src="./storage/img/pngwing.com (2).png" alt="" data-id="${cap.id}" class="completado">
+                    </div>
+                    <div class="eliminado">
+                        <img src="./storage/img/pngwing.com (4).png" data-id="${cap.id}" alt="" class="eliminado">
+                    </div>
+
+                </div>
+            </div>`
             }
             datosContenedor.appendChild(capDiv);
         })
