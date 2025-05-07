d9388521b2645f2bf96332186c3df9847f923fca
saavedra.mariaju@gmail.com <saavedra.mariaju@gmail.com>
2025-05-07 09:03:15 -0500

    Feat :sparkles: new update principal page html

---
+++ ApiSuperHeroes/script/script.js
@@ -0,0 +1,21 @@
+async function NuevoHeroe(){
+
+}
+
+
+
+
+
+
+
+
+
+const inputs= document.getElementById("nuevo");
+inputs.addEventListener("click",function(){
+    NuevoHeroe()
+})
+
+const inputss= document.getElementById("fondo__formBusqueda--next");
+inputss.addEventListener("click",function(){
+    NuevoHeroe()
+})
