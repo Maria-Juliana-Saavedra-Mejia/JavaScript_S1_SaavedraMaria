5ca0724aa2ff543ff6e73b5dcd0efe69f6ff8ee2
saavedra.mariaju@gmail.com <saavedra.mariaju@gmail.com>
2025-05-14 09:06:47 -0500

    Feat :sparkles: new update PacoPets

--- ApiRefugio/script/script.js
+++ ApiRefugio/script/script.js
@@ -27,7 +27,7 @@ function almacenamiento(){
         .then(data =>{
             for (i=0;i<data.animals.length;i++){
                 let mascotas = data.animals[i]
-                let imagesrc = mascotas.photos[0]?.full || mascotas.photos[0]?.large || mascotas.photos[0]?.medium || mascotas.photos[0]?.small || 'https://media.istockphoto.com/id/1296353202/es/foto/grupo-de-mascotas-posando-alrededor-de-un-collie-fronterizo-perro-gato-hur%C3%B3n-conejo-p%C3%A1jaro-pez.jpg?s=612x612&w=0&k=20&c=L2IMPfImJLLGeXTYUIpoFJ9JrnOjHvxgzr-sYbtAZaQ=';
+                let imagesrc = mascotas.photos[0]?.full || mascotas.photos[0]?.large || mascotas.photos[0]?.medium || mascotas.photos[0]?.small || 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png';
                 let age= mascotas.age
                 let gender= mascotas.gender
                 let size=mascotas.size
@@ -38,14 +38,13 @@ function almacenamiento(){
                 let especie= mascotas.species
                 cpets.innerHTML += `
                 <div class="ContenedorAnimales">
-                  <img src="../img/estrellaVacia.svg" class="simboloMas">
-                  <img src="../img/mas.svg" class="simboloEstrella">
                   <img class="PetImages" src="${imagesrc}"/>
                   <h1 class="PetName">Nombre: ${mascotas.name}</h1>
                   <h1 class="age">Edad y Tamaño: ${age}, ${size}</h1>
-                  <h1 class="location">Ubicación: ${ubicacion}, ${country}, ${city}</h1>
+                  <h1 class="location">Ubicación:  ${country}, ${city}</h1>
                   <h1 class="breed">Especie, Genero y Raza: ${especie}, ${gender}, ${raza}</h1>
-                  
+                  <img src="../img/mas.svg" class="simboloEstrella">
+                  <img src="../img/estrellaVacia.svg" class="simboloMas">
                 </div>
                 `;
             }
